7968bfb63261fe65b7cad638764188429da39ba7
shahzad <mohmdshahzad2.0@gmail.com>
2025-06-10 10:48:07 +0530

    added logo for the app

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -9,7 +9,7 @@ export const injectStore = (_store: Store<RootState>) => {
 };
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api/",
   withCredentials: true,
 });
 
